1e9e542b9a26a158154e92aed12f632c26d2dab6
Altsniper <132731972+Altsniper@users.noreply.github.com>
2023-08-24 14:38:57 +0900

    springboot init

---
+++ wont/home1/src/main/java/com/waterflat/home1/Home1Application.java
@@ -0,0 +1,13 @@
+package com.waterflat.home1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Home1Application.class, args);
+	}
+
+}
